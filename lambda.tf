e939306ccebe7f47dd6deced2e7de168a19417f3
Pablo Guillamondegui <pablo@zoocha.com>
2025-04-02 17:42:46 +0100

    Adapt lambda resources to support multiregion

--- lambda.tf
+++ lambda.tf
@@ -1,11 +1,21 @@
-resource "aws_cloudwatch_log_group" "lambda-log-group" {
-  name  = "UpdateCloudflareIps"
-  count = var.enabled == "true" ? 1 : 0
+locals {
+  resource_count = var.enabled == "true" ?
+    (var.multiple_region_enabled && length(var.multiple_region_list) > 0 ? length(var.multiple_region_list) : 1)
+    : 0
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  count = local.resource_count
+  name  = var.multiple_region_enabled && length(var.multiple_region_list) > 0 ?
+    "UpdateCloudflareIps-${var.multiple_region_list[count.index]}" :
+    "UpdateCloudflareIps"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  count              = var.enabled == "true" ? 1 : 0
-  name               = "lambda-cloudflare-role"
+  count              = local.resource_count
+  name               = var.multiple_region_enabled && length(var.multiple_region_list) > 0 ?
+    "lambda-cloudflare-role-${var.multiple_region_list[count.index]}" :
+    "lambda-cloudflare-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -24,10 +34,12 @@ EOF
 }
 
 resource "aws_iam_policy" "policy" {
-  name        = "lambda-cloudflare-policy"
-  count       = var.enabled == "true" ? 1 : 0
-  description = "Allows cloudflare ip updating lambda to change security groups"
-  policy      = <<EOF
+  count              = local.resource_count
+  name               = var.multiple_region_enabled && length(var.multiple_region_list) > 0 ?
+    "lambda-cloudflare-policy-${var.multiple_region_list[count.index]}" :
+    "lambda-cloudflare-policy"
+  description        = "Allows cloudflare ip updating lambda to change security groups"
+  policy             = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -61,25 +73,26 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "policy" {
-  count      = var.enabled == "true" ? 1 : 0
-  role       = aws_iam_role.iam_for_lambda[0].id
-  policy_arn = aws_iam_policy.policy[0].arn
+  count      = local.resource_count
+  role       = aws_iam_role.iam_for_lambda[count.index].id
+  policy_arn = aws_iam_policy.policy[count.index].arn
 }
 
 data "archive_file" "lambda_zip" {
-  count       = var.enabled == "true" ? 1 : 0
+  count       = local.resource_count
   type        = "zip"
   source_file = "${path.module}/cloudflare-security-group.py"
-  output_path = "${path.module}/lambda.zip"
+  output_path = "${path.module}/lambda-${var.multiple_region_enabled && length(var.multiple_region_list) > 0 ? var.multiple_region_list[count.index] : "default"}.zip"
 }
 
+
 resource "aws_lambda_function" "update-ips" {
-  count            = var.enabled == "true" ? 1 : 0
-  function_name    = "UpdateCloudflareIps"
+  count            = local.resource_count
+  function_name    = "UpdateCloudflareIps-${var.multiple_region_enabled && length(var.multiple_region_list) > 0 ? var.multiple_region_list[count.index] : "default"}"
   filename         = "${path.module}/lambda.zip"
   source_code_hash = data.archive_file.lambda_zip[0].output_base64sha256
   handler          = "cloudflare-security-group.lambda_handler"
-  role             = aws_iam_role.iam_for_lambda[0].arn
+  role             = aws_iam_role.iam_for_lambda[count.index].arn
   runtime          = "python3.9"
   timeout          = 60
   environment {
@@ -88,4 +101,3 @@ resource "aws_lambda_function" "update-ips" {
     }
   }
 }
-
