478c15b9b5d2d477078ecd340ec947d803b4e85d
Pablo Guillamondegui <pablo@zoocha.com>
2025-04-02 17:42:02 +0100

    Create the variables to check if the configuration is multi-regional

--- vars.tf
+++ vars.tf
@@ -5,6 +5,14 @@ variable "enabled" {
   description = "Whether to do anything at all, useful if cloudflare is not needed on all environments. Accepts the string 'true' or 'false'."
   default     = "true"
 }
+variable "multiple_region_enabled" {
+  description = "Check whether or not resources will be created in multiple regions."
+  default     = false
+}
+variable "multiple_region_list" {
+  description = "List for the multiple region setup."
+  default = []
+}
 variable "schedule_expression" {
   description = "The cloudwatch schedule expression used to run the updater lambda."
   default     = "cron(0 20 * * ? *)"
